7ddb59c24801744bb27894ff8f136d5e86383a16
153 <52@4x13.net>
2018-09-01 13:31:58 -0700

    Update lm.py
    Improved "lm.py" parser so that lispmark can be processed from the command line
    
     eg "./lm.py file.lm" outputs "file.html"

--- lm.py
+++ lm.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 # simple parse tool to generate .html from .lm file 
-
+import sys
 import parse
 
 def main():
-        fn = input("file: ")
+        if len(sys.argv) == 1:
+            fn = input("file: ")
+        else:
+            fn = sys.argv[1]
         with open(fn + ".lm", "r") as f:
-	        f = f.read()
+            f = f.read()
         f = "".join(["(html", f, ")"])
         lm = parse.eval_input(f)
-        print(lm)
-        wri = input("Write file?")
-        if wri not in ["0", "n"]:
-	        with open(fn + ".html", "w") as nf:
-		        nf.write(lm)
+        if len(sys.argv) == 1:
+            print(lm)
+            wri = input("Write file?")
+            if wri not in ["0", "n"]:
+                with open(fn + ".html", "w") as nf:
+                    nf.write(lm)
+        else:
+            with open(fn + ".html", "w") as nf:
+                nf.write(lm)
+
+main()
